99569e49bbce11689ed8273421632657bfed7a05
exabugs <exabugs@gmail.com>
2023-06-04 15:21:27 +0900

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
+import express from "express";
 
-const a = (x: string) => {
+const log = (x: string) => {
   console.log(x);
 };
 
-console.log(a("hello"));
-
-axios({
-  url: "https://jsonplaceholder.typicode.com/todos",
-  params: { _limit: 3 },
-}).then((a) => {
-  console.log(JSON.stringify(a.data, null, "  "));
+const app = express();
+app.get("/todos", (_req, res) => {
+  axios({
+    url: "https://jsonplaceholder.typicode.com/todos",
+    params: { _limit: 3 },
+  }).then(({ data }) => {
+    res.type("json");
+    res.json({ data });
+  });
 });
+
+app.listen(3000);
+log("http://localhost:3000/todos");
